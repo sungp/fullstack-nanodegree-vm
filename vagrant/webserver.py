0f77fbdc10fddcf16c9a1a7518ec3fda9b72d648
Sung <sungp2k@gmail.com>
2017-10-07 08:16:48 -0700

    object 4 completed

--- vagrant/webserver.py
+++ vagrant/webserver.py
@@ -49,36 +49,52 @@ class webServerHandler(BaseHTTPRequestHandler):
                 restaurants = session.query(Restaurant).all()
 
                 output = ""
-                output = "<a href = 'restaurant/new'> Make a New Restaruant </a></br>"
+                output = "<a href = 'restaurants/new'> Make a New Restaruant </a></br>"
                 output += "<html><body>"
                 for restaurant in restaurants:
                   output += "</br></br>{}</br>".format(restaurant.name)
-                  output += "<a href = '#'>Edit</a> </br>"
+                  output += "<a href = 'restaurants/%d/edit'>Edit</a> </br>" % restaurant.id
                   output += "<a href = '#'>Delete</a>"
 
                 output += "</body></html>"
                 self.wfile.write(output)
                 return
 
-            if self.path.endswith("/restaurant/new"):
+            if self.path.endswith("/restaurants/new"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 output = ""
                 output += "<html><body>"
                 output += "<h1>Make a New Restaurant</h1>"
-                output += '''<form method='POST' enctype='multipart/form-data' action='/restaurant/new'><input name="newRestaurantName" type="text"  placeholder = 'New Restaurant Name'><input type="submit" value="Create"> </form>'''
+                output += '''<form method='POST' enctype='multipart/form-data' action='/restaurants/new'><input name="newRestaurantName" type="text"  placeholder = 'New Restaurant Name'><input type="submit" value="Create"> </form>'''
                 output += "</body></html>"
                 self.wfile.write(output)
                 print output
                 return                 
+            
+            if self.path.endswith("/edit"):
+                restid = self.path.split('/')[2]
+                rest_update = session.query(Restaurant).filter_by(id = int(restid)).first()
+                print "updating " + rest_update.name
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = ""
+                output += "<html><body>"
+                output += "<h1>%s</h1>" % rest_update.name
+                output += '''<form method='POST' enctype='multipart/form-data' action='/restaurants/{}/edit'><input name="rename" type="text"  placeholder = '{}'><input type="submit" value="Rename"> </form>'''.format(restid, rest_update.name)
+                output += "</body></html>"
+                self.wfile.write(output)
+                print output
+
 
         except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
 
     def do_POST(self):
         try:
-          if self.path.endswith("/restaurant/new"):
+          if self.path.endswith("/restaurants/new"):
             ctype, pdict = cgi.parse_header(
                 self.headers.getheader('content-type'))
             output = ""
@@ -94,6 +110,28 @@ class webServerHandler(BaseHTTPRequestHandler):
               self.send_header('Content-type', 'text/html')
               self.send_header('Location', '/restaurants')
               self.end_headers()
+          
+          if self.path.endswith("/edit"):
+            ctype, pdict = cgi.parse_header(
+                self.headers.getheader('content-type'))
+            restid = self.path.split('/')[2]
+            rest_update = session.query(Restaurant).filter_by(id = int(restid)).first()
+            print "restaurant to be updated: %s" % rest_update.name
+            output = ""
+            if ctype == 'multipart/form-data':
+              fields = cgi.parse_multipart(self.rfile, pdict)
+              messagecontent = fields.get('rename')
+              print fields 
+              print "restaurant to be updated:from {} to {}".format(rest_update.name, messagecontent[0])
+              rest_update.name = messagecontent[0]
+              session.add(rest_update)
+              session.commit()
+
+              self.send_response(301)
+              self.send_header('Content-type', 'text/html')
+              self.send_header('Location', '/restaurants')
+              self.end_headers()
+
         except:
             pass
 
