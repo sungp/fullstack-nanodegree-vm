5f62b795ea92080bcf3dfbcdcc75211b2ac8bb11
Sung <sungp2k@gmail.com>
2017-10-10 07:21:34 -0700

    edit menu item quiz

--- vagrant/project.py
+++ vagrant/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
