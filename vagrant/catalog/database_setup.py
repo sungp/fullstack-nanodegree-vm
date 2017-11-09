141c93067645cb71a743fab6d51df8756d495304
Sung <sungp2k@gmail.com>
2017-11-09 06:57:55 -0800

    login works

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -1,12 +1,19 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
 
 class Category(Base):
     __tablename__ = 'category'
@@ -23,6 +30,9 @@ class Item(Base):
     description = Column(String(250))
     cat_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    date_added = Column(DateTime)
 
 # We added this serialize function to be able to send JSON objects in a
 # serializable format
