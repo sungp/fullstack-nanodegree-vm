e488d6f395d05d240f7e5e08f0823451c98f67c3
Sung <sungp2k@gmail.com>
2017-11-09 07:51:58 -0800

    login works

--- vagrant/catalog/project.py
+++ vagrant/catalog/project.py
@@ -182,22 +182,7 @@ def gdisconnect():
         return response
 
 
-# JSON APIs to view Restaurant Information
-@app.route('/restaurant/<int:restaurant_id>/menu/JSON')
-def restaurantMenuJSON(restaurant_id):
-    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    items = session.query(MenuItem).filter_by(
-        restaurant_id=restaurant_id).all()
-    return jsonify(MenuItems=[i.serialize for i in items])
-
-
-@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/JSON')
-def menuItemJSON(restaurant_id, menu_id):
-    Menu_Item = session.query(MenuItem).filter_by(id=menu_id).one()
-    return jsonify(Menu_Item=Menu_Item.serialize)
-
-
-@app.route('/restaurant/JSON')
+@app.route('/catalog/JSON')
 def restaurantsJSON():
     restaurants = session.query(Restaurant).all()
     return jsonify(restaurants=[r.serialize for r in restaurants])
@@ -205,11 +190,13 @@ def restaurantsJSON():
 
 # Show all categories 
 @app.route('/')
-@app.route('/category')
+@app.route('/catalog')
 def showCatalog():
     categories = session.query(Category).order_by(asc(Category.name))
-    return render_template('catalog.html', categories=categories)
-
+    if 'username' not in login_session:
+      return render_template('public_catalog.html', categories=categories)
+    else:
+      return render_template('catalog.html', categories=categories)
 # Create a new restaurant
 
 
@@ -266,7 +253,7 @@ def deleteRestaurant(restaurant_id):
 
 # Show items 
 
-@app.route('/category/<int:category_id>/item/')
+@app.route('/catalog/<int:category_id>/item/')
 def showItem(category_id):
   return "category id = %d" % {category_id}
 
