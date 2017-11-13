9e6d1759b41610e466652cae901e17dba66d85a0
Sung <sungp2k@gmail.com>
2017-11-13 08:19:23 -0800

    login logout page work with appropriate flash message

--- vagrant/catalog/project.py
+++ vagrant/catalog/project.py
@@ -160,26 +160,30 @@ def gdisconnect():
     # Only disconnect a connected user.
     access_token = login_session.get('access_token')
     if access_token is None:
-        response = make_response(
-            json.dumps('Current user not connected.'), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
-    h = httplib2.Http()
-    result = h.request(url, 'GET')[0]
-    if result['status'] == '200':
-        del login_session['access_token']
-        del login_session['gplus_id']
-        del login_session['username']
-        del login_session['email']
-        del login_session['picture']
+      response = make_response(
+          json.dumps('Current user not connected.'), 401)
+      flash("You weren't loggen in.") 
+    else: 
+      url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
+      h = httplib2.Http()
+      result = h.request(url, 'GET')[0]
+      if result['status'] == '200':
         response = make_response(json.dumps('Successfully disconnected.'), 200)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-    else:
+      else:
         response = make_response(json.dumps('Failed to revoke token for given user.', 400))
-        response.headers['Content-Type'] = 'application/json'
-        return response
+      flash("You have successfully been logged out.")
+    
+    response.headers['Content-Type'] = 'application/json'
+    login_session.pop('access_token', None)
+    login_session.pop('picture', None)
+    login_session.pop('gplus_id', None)
+    login_session.pop('username', None)
+    login_session.pop('email', None)
+    login_session.pop('picture', None)
+    login_session.pop('user_id', None)
+    login_session.pop('provider', None)
+    return redirect(url_for('showCatalog'))
+
 
 
 @app.route('/catalog/JSON')
