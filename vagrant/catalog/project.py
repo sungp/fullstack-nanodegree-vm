474f0b5015860a40b6a097c0f1fd6d48b22b574a
Sung <sungp2k@gmail.com>
2017-11-22 06:33:09 -0800

    cleanup

--- vagrant/catalog/project.py
+++ vagrant/catalog/project.py
@@ -320,4 +320,4 @@ def deleteItem(category_name, item_title):
 if __name__ == '__main__':
   app.secret_key = 'super_secret_key'
   app.debug = True
-  app.run(host='0.0.0.0', port=5000)
+  app.run(host='0.0.0.0', port=8000)
